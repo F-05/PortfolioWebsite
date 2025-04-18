447de98f15c0426538775d180e3472aa49b0d8cd
fideris susanto <fsus8677@uni.sydney.edu.au>
2025-04-18 15:30:21 +1000

    added assets and images for the website and starting to create the home page

---
+++ my-app/src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/fiderissusanto/" className="home__social-icon" target="#">
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/in/fideris-susanto-047864213/" className="home__social-icon" target="#">
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/F-05" className="home__social-icon" target="#">
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
