e677ebcb951109af4782f7b27aa05c972fbfd023
fideris susanto <fsus8677@uni.sydney.edu.au>
2026-04-06 23:47:50 +1000

    fix: separate qualification education and experience tabs

--- my-app/src/components/qualification/Qualification.jsx
+++ my-app/src/components/qualification/Qualification.jsx
@@ -46,15 +46,10 @@ const Qualification = () => {
                             <span className="qualification__line"></span>
                         </div>
                     </div>
+                </div>
 
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-
                         <div>
                             <h3 className="qualification__title">Mathematics Tutor</h3>
                             <span className="qualification__subtitle">Seriously Addictive Mathematics</span>
@@ -62,9 +57,12 @@ const Qualification = () => {
                                 <i className="uil uil-calendar-alt"></i> 2023 - Present
                             </div>
                         </div>
-                    </div>
 
-                    
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
